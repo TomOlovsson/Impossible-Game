e347fb640936ce1af6e04d12e3a0a33ea9112fec
TomOlovsson <tom.olovsson@student.forsbergsskola.se>
2021-09-14 13:04:09 +0200

    Added a C# movement script

---
+++ Assets/Movement/rvmovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rvmovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody rb;
+    public Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+    }
+    
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+        void moveCharacter(Vector2 direction)
+    {
+            rb.AddForce(direction * speed);
+    }
+}
+}
